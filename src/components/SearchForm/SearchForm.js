fa09378c8fa44011ff23e6aab76b906d513b0c48
Mateusz Korczak <korczak.programowanie@gmail.com>
2022-01-11 13:20:51 +0100

    fixed bug with wrong navigate page, add column problem. Transfered SearchForm into List component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,12 +2,19 @@ import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
   const [title, setTitle] = useState('');
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(updateSearchString(title));
+    dispatch({
+      type: 'UPDATE_SEARCHSTRING',
+      payload: title,
+    });
+  }, []);
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(updateSearchString(title));
