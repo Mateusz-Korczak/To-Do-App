40bf0a0e820994c0f9e63f5686339c0ef90acee2
Mateusz Korczak <korczak.programowanie@gmail.com>
2022-01-12 13:51:49 +0100

    partition reducer into subreducers with separatet components modules

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,8 +1,22 @@
 import shortid from 'shortid';
+import strContains from '../utils/strContains';
+
+//selectors:
+export const getFilteredCards = ({ cards, searchString }, columnId) =>
+  cards.filter(
+    (card) =>
+      card.columnId === columnId && strContains(card.title, searchString)
+  );
+
+// actions:
+const createActionName = (actionName) => `app/lists/${actionName}`;
+const ADD_CARD = createActionName('ADD_CARD');
+//creators:
+export const addCard = (payload) => ({ type: ADD_CARD, payload });
 
 const cardsReducer = (statePart = [], action) => {
   switch (action.type) {
-    case 'ADD_CARD':
+    case ADD_CARD:
       return [...statePart, { ...action.payload, id: shortid() }];
     case 'TOGGLE_CARD_FAVORITE':
       return statePart.map((card) =>
